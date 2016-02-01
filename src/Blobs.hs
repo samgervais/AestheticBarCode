2efdcf1049851ebf5c910d504cef4fe5a55a7d54
Jordan Medlock <jordanemedlock@gmail.com>
2016-01-31 20:17:48 -0700

    Fixed the bounds issue

--- src/Blobs.hs
+++ src/Blobs.hs
@@ -13,11 +13,15 @@ xss = [[0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 nss = array ((0,0),(12,7)) [((x,y), xss !! y !! x) | x <- [0..12], y <- [0..7]]
 
+inBounds :: Array (Int, Int) Int -> (Int,Int) -> Bool
+inBounds arr (x,y) = x >= lx && x <= hx && y >= ly && y <= hy
+  where ((lx,ly), (hx,hy)) = bounds arr
+
 checkAround :: Array (Int, Int) Int -> (Int,Int) -> [(Int,Int)]
-checkAround arr (x,y) = [ (x',y') | (i,j) <- [(0,1),(1,0),(-1,0),(0,-1)]
-                        , let x' = x + i
-                        , let y' = y + j
-                        , arr ! (x', y') == 1
+checkAround arr (x,y) = [ (x',y') | (h,k) <- [(0,1),(1,0),(-1,0),(0,-1)]
+                        , let x' = x + h
+                        , let y' = y + k
+                        , inBounds arr (x',y') && arr ! (x',y') == 1
                         ]
 
 getAllConnected' :: Array (Int, Int) Int -> [(Int,Int)] -> [(Int,Int)] -> [(Int,Int)]
@@ -33,7 +37,7 @@ replaceWith x arr index = arr//[(i,x) | i <- index]
 
 perIndex :: (Array (Int, Int) Int, [[(Int,Int)]]) -> (Int,Int) -> (Array (Int, Int) Int, [[(Int,Int)]])
 perIndex (arr, xss) index = case arr ! index of
-                          1 -> (replaceWith 971 arr newXs, newXs : xss) 
+                          1 -> (replaceWith 971 arr newXs, newXs : xss)
                           x -> (arr, xss)
                           where newXs = getAllConnected arr index
 
